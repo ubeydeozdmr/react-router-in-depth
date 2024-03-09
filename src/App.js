e0b4204c3903c6f0257654c26f03487dc8dbcb5a
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-09 23:57:30 +0300

    Nested Routes & Layouts

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import {
   createBrowserRouter,
-  Route,
   createRoutesFromElements,
+  Route,
   RouterProvider,
 } from 'react-router-dom';
 
 // pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Faq from './pages/Help/Faq';
+import Contact from './pages/Help/Contact';
 
 // layouts
 import RootLayout from './layouts/RootLayout';
+import HelpLayout from './layouts/HelpLayout';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Route>,
   ),
 );
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 
 export default function RootLayout() {
   return (
@@ -8,6 +8,7 @@ export default function RootLayout() {
           <h1>Jobarouter</h1>
           <NavLink to="/">Home</NavLink>
           <NavLink to="about">About</NavLink>
+          <NavLink to="help">Help</NavLink>
         </nav>
       </header>
 
