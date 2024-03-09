f35b55ed28bf750f516c0a488fb3980c4c1f77a5
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-09 15:52:02 +0300

    Introduction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+  return <div className="App">Hello, Ninjas!</div>;
 }
 
-export default App
+export default App;
