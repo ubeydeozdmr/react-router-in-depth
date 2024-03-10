01a7fa7a71c7319b365bbe1b721141c6a1a36b20
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-10 13:55:07 +0300

    Loaders

--- src/App.js
+++ src/App.js
@@ -15,15 +15,22 @@ import Contact from './pages/Help/Contact';
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
 import NotFound from './pages/NotFound';
+import CareersLayout from './layouts/CareersLayout';
+import Careers, { careersLoader } from './pages/Careers/Careers';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} loader={careersLoader} />
       </Route>
 
       <Route path="*" element={<NotFound />} />
