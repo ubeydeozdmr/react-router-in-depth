027b41af5e7e815a7f5365c9ed89e06e0d07ff61
Ubeyde Emir Özdemir <ubeydeozdmr@gmail.com>
2024-03-11 13:14:32 +0300

    Making Breadcrumbs (useLocation hook)

---
+++ src/components/Breadcrumbs.js
@@ -0,0 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
+
+export default function Breadcrumbs() {
+  const location = useLocation();
+
+  let currentLink = '';
+
+  const crumbs = location.pathname
+    .split('/')
+    .filter((crumb) => crumb !== '')
+    .map((crumb) => {
+      currentLink += `/${crumb}`;
+      return (
+        <div className="crumb" key={currentLink}>
+          <Link to={currentLink}>{crumb}</Link>
+        </div>
+      );
+    });
+
+  return <div className="breadcrumbs">{crumbs}</div>;
+}
